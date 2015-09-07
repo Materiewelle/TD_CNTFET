7f7acd492649cf3d3c1497dae08e08a6d3feba34
Fabian Motzfeld <fabian.motzfeld@rwth-aachen.de>
2015-09-07 17:02:19 +0200

    for the cluster

--- device_params.hpp
+++ device_params.hpp
@@ -65,15 +65,6 @@ public:
     inline void update(const std::string & n);
 };
 
-static const device_params nfet("nfet", fet_geometry, nfet_model);
-static const device_params nfetc("nfetc", fet_geometry, nfetc_model);
-static const device_params pfet("pfet", fet_geometry, pfet_model);
-static const device_params ntfet("ntfet", tfet_geometry, ntfet_model);
-static const device_params ntfetc("ntfetc", tfetc_geometry, ntfetc_model);
-static const device_params ntfetn("ntfetn", tfetn_geometry, ntfet_model);
-static const device_params ntfetnc("ntfetnc", tfetn_geometry, ntfetc_model);
-static const device_params ptfet("ptfet", tfet_geometry, ptfet_model);
-
 //----------------------------------------------------------------------------------------------------------------------
 
 device_params::device_params(const std::string & n_, const geometry & g_, const model & m_)
