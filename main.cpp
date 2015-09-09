0a1a33f8d34d8c4518f716080330705cef76613d
Fabian Motzfeld <fabian.motzfeld@rwth-aachen.de>
2015-09-09 21:44:01 +0200

    And another one...

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ static const geometry tfet_geometry {
     10.0, // eps_cnt
     25.0, // eps_ox
     10.0, // l_sc
-    15.0, // l_sox
+    20.0, // l_sox
      5.0, // l_sg
     30.0, // l_g
     30.0, // l_dg
